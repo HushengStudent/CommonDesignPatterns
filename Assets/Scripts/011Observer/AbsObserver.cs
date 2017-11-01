3b38573826e5eb4caf9a5a716f2c0637e3452955
胡胜同学 <husheng.student@foxmail.com>
2017-11-01 23:37:27 +0800

    Observer Pattern

---
+++ Assets/Scripts/011Observer/AbsObserver.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 观察者模式
+/// </summary>
+public abstract class Observer 
+{
+    public abstract void Update();
+}
---
+++ Assets/Scripts/011Observer/AbsSubject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 通知
+/// </summary>
+public abstract class AbsSubject 
+{
+    public abstract void Notify();
+}
